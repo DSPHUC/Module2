f7760c7078cce88b90d42d5154a0c852c90f0853
DSPHUC <phuc.dongsi@gmail.com>
2023-06-09 10:47:59 +0700

    add file find max numbẻ in array

---
+++ Practice/src/FindMaxArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FindMaxArray {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Nhập độ dài mảng:");
+        int size = scanner.nextInt();
+        int[] arrNumber = new int[size];
+        int i = 0;
+        while (i < size) {
+            System.out.println("Nhập số tại vị trí: " + i);
+            arrNumber[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.println("Mảng :");
+        for (int j : arrNumber) {
+            System.out.print(j + "\t");
+        }
+        int maxNum = arrNumber[0];
+        int count = 0;
+        for (int j = 1; j < size; j++) {
+            if (maxNum < arrNumber[j]) {
+                maxNum = arrNumber[j];
+                count = j;
+            }
+        }
+        System.out.println("Số lớn nhất trong mảng là :" + maxNum);
+        System.out.println("Số lớn nhất trong mảng là :" + count);
+    }
+}
