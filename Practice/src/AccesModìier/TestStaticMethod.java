a2c3a5e2440a80549192782aa2bf55a619f569fb
DSPHUC <phuc.dongsi@gmail.com>
2023-06-14 10:39:23 +0700

    move all file and add file practice static method

---
+++ Practice/src/AccesModìier/TestStaticMethod.java
@@ -0,0 +1,14 @@
+package AccesModìier;
+
+public class TestStaticMethod {
+    public static void main(String[] args) {
+        StaticMethod.change();
+        StaticMethod s1 = new StaticMethod(111, "Nam"); 
+        StaticMethod s2 = new StaticMethod(222, "Trung");
+        StaticMethod s3 = new StaticMethod(333, "Cường");
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+
+}
