d7794690daa28ffd5a06433299b0fd9272c6d4d2
DSPHUC <phuc.dongsi@gmail.com>
2023-06-15 10:01:14 +0700

    add file point 2D & 3D

---
+++ Exercise/src/Inheritance/PoinTest.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class PoinTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(5,7);
+        System.out.println(point2D);
+        Point3D point3D =new Point3D(7,7,7);
+        System.out.println(point3D);
+    }
+
+
+}
