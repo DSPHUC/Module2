a3a2e2be90ef1d185333b9eace7407b4bc14192b
DSPHUC <phuc.dongsi@gmail.com>
2023-06-16 16:18:23 +0700

    move file resizeable

---
+++ Exercise/src/AbstractAndInterface/Resizeable/Resizeable.java
@@ -0,0 +1,5 @@
+package AbstractClass_And_Interface.InterFace_Assignment_GeometryClass.Resizeable;
+
+public interface Resizeable {
+    public abstract void resize(double percent);
+}
---
+++ Exercise/src/AbstractAndInterface/Resizeable/ResizeableTest.java
@@ -0,0 +1,20 @@
+package AbstractClass_And_Interface.InterFace_Assignment_GeometryClass.Resizeable;
+
+public class ResizeableTest {
+    public static void main(String[] args) {
+        int x = (int) Math.floor(Math.random() * 10);
+
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(x, "red", false);
+        shapes[1] = new Square(x, "blue", false);
+        shapes[2] = new Rectangle(x, x, "orange", true);
+        for (Shape shape : shapes) {
+            System.out.println(shape);
+        }
+        for (Shape shape : shapes) {
+            shape.resize(Math.random() * 1000);
+        }
+        System.out.println("\n");
+        Shape.printShape(shapes);
+    }
+}
