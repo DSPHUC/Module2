284412eb87ea37a8e0b2da38c03341ddd0f2f430
DSPHUC <phuc.dongsi@gmail.com>
2023-06-26 08:46:21 +0700

    add file check brackets

--- Exercise/src/Stack_Queue/Check_Brackets/Use_Stack.java
+++ Exercise/src/Stack_Queue/Check_Brackets/Use_Stack.java
@@ -1,2 +1,31 @@
-package Stack_Queue.Check_Brackets;public class Use_Stack {
+package Stack_Queue.Check_Brackets;
+
+import java.util.Stack;
+
+public class Use_Stack {
+    public static boolean check(String s) {
+
+        Stack<Character> bStack = new Stack<>();
+        for (char sym : s.toCharArray()) {
+            if (sym == '(' || sym == '{' || sym == '[') {
+                bStack.push(sym);
+            } else if (sym == ')' || sym == '}' || sym == ']') {
+//                bStack.push(sym);
+
+                if (bStack.isEmpty()) {
+                    return false;
+                }
+                char left = bStack.pop();
+                if (left == '(' && sym != ')' || left == '{' && sym != '}' || left == '[' && sym != ']') {
+                    return false;
+                }
+            }
+
+        }
+        return bStack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check("5*2+6*(3*2-4))*((9+4/2)*2(7*(2+5))"));
+    }
 }
